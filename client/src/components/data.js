508bae31652c149997176fe71d6da7fa7cb0e471
Precious <Preshomondi@gmail.com>
2022-10-13 00:59:24 +0300

    create quotes data

---
+++ client/src/components/data.js
@@ -0,0 +1,9 @@
+const quotes = [
+  "Work on you for you",
+  "I work out because I think my great personality could use a banging body",
+  "Quit the shit, get fit!",
+  "Find your fire",
+  "Be stronger than your excuse"
+]
+
+export {quotes}
