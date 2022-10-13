c11bcf9b8d688d4268ace8389455a2215d1c4c1d
Precious <Preshomondi@gmail.com>
2022-10-14 01:24:50 +0300

    style Trainer component

--- client/src/components/Trainer.js
+++ client/src/components/Trainer.js
@@ -12,16 +12,17 @@ function Trainer() {
                     <div className="trainer-image">
                         <img src="https://images.pexels.com/photos/7991680/pexels-photo-7991680.jpeg?auto=compress&cs=tinysrgb&w=400" />
                     </div>
-                    <h3 className="trainer-name">Marlin</h3>
+                    <h2 className="trainer-name">Marlin</h2>
                     <p className="trainer-specialty">Calisthenics</p>
-                    <p className="trainer-bio">
-                    An expert with 4 years experience in Yoga and martial arts
-                    </p>
+                </div>
+                <div className="trainer-bio">
+                    <p>An expert with 4 years experience in Yoga and martial arts</p>
+                    <div className="trainer-trainees">
                     <h4>Trainees:</h4>
                     <p>2</p>
-
-
-                </div>
+                    </div>
+                    
+                </div>        
             </div>
         </div>
     )
