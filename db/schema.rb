55b23603cf37d15304531537e74e830ad518c89c
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-10-13 20:49:03 +0300

    all

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_190318) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_125823) do
   create_table "programs", force: :cascade do |t|
     t.string "name"
     t.integer "duration"
@@ -43,6 +43,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_190318) do
     t.string "specialization"
     t.string "background"
     t.integer "no_of_trainees"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
