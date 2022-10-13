85f515d420e28d4f1ae104edeba4b640dddcd820
Precious <Preshomondi@gmail.com>
2022-10-13 10:50:52 +0300

    add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
-
+import React, { useEffect, useState } from "react"
 import './App.css';
 import Signup from './components/Signup';
 import Home from './components/Home'
 import Sidebar from './components/Sidebar';
+import { Route, Switch } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path="/home">
+          <Sidebar/>
+          <Route path="/home/main">
+            <Home/>
+          </Route>
+        </Route>
+        <Route  exact path={"/"}>
+          <Signup />
+        </Route>
+        
+
+      </Switch>
      
-       <Sidebar/>     
+        
     </div>
   );
 }
