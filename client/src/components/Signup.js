c7b9e84ee28b8ed30f9f8f0b08366b20fb63803f
Precious <Preshomondi@gmail.com>
2022-10-13 04:32:47 +0300

    style signup page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,28 +1,63 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import "./Signup.css"
+import { quotes } from "./data"
 
-function Signup(){
-    return(
-        <div className="signup">
-            <div className="container">
-                <div className="left" style={{ backgroundImage: `url("https://images.pexels.com/photos/4944311/pexels-photo-4944311.jpeg?auto=compress&cs=tinysrgb&w=400")` }} >
-                    <div className="left-blur">
-                        <h2>Just Gym-It</h2>
-                        <h3>Sign Up Here</h3>
-                        <form>
-                        <input type="text" placeholder='First name' value={signupDetails.first_name} name="first_name" onChange={handleChange}></input>
-                         <input type="password" placeholder='Password' required value={signupDetails.password} name="password" onChange={handleChange}></input>
-                        <input type="password" placeholder='Confirm Password' required value={confirmPassword} name="confirm_password" onChange={handleConfirm} onBlur={handleCheckMatch}></input>
-                        <input type="text" placeholder='City' required value={signupDetails.city} name="city" onChange={handleChange}></input>
-                        <input type="text" placeholder='Field of Work' value={signupDetails.job} name="job" onChange={handleChange}></input>
-                        <input type="date" value={signupDetails.birth} name="birth" onChange={handleChange} />
- 
+function Signup() {
+    
 
-                        </form>
+    return (
+        <div className="signup" style={{ backgroundImage: `url("https://images.pexels.com/photos/4944311/pexels-photo-4944311.jpeg?auto=compress&cs=tinysrgb&w=400")` }} >
+
+            <div className="left-blur" >
+                <div className="left-container">
+                    <h1>Just Gym-It</h1>
+                    
+                    <form>
+                        <input type="text" placeholder="Username" name="username" />
+                        <input type="password" placeholder="Password" name="password" />
+                        <select name="gender">
+                            <option value="">--Select gender--</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                        <select name="age">
+                            <option value="">--Select age--</option>
+                            <option value="18-25">18-25</option>
+                            <option value="26-35">26-35</option>
+                            <option value="36-45">36-45</option>
+                            <option value="46 and above">46 and above</option>
+                        </select>
+                        <select name="weight">
+                            <option value="">--Select weight--</option>
+                            <option value="18-25">18-25</option>
+                            <option value="26-35">26-35</option>
+                            <option value="36-45">36-45</option>
+                            <option value="46 and above">46 and above</option>
+                        </select>
+                        <select name="height">
+                            <option value="">--Select height--</option>
+                            <option value="18-25">18-25</option>
+                            <option value="26-35">26-35</option>
+                            <option value="36-45">36-45</option>
+                            <option value="46 and above">46 and above</option>
+                        </select>
+                        <button className="signup-btn">Sign Up</button>
+                       
+                    </form>
+                </div>
+            </div>
+            <div className="right-blur">
+                <div className="right-container">
+                    <div className="right-text">
+                    <span>“</span>
+                    <h2>{quotes[Math.floor(Math.random() * quotes.length)]}</h2>
                     </div>
 
+                    <div className="right-text-login">
+                        <p>Sign In Here</p>
+                    </div>
+                    
                 </div>
-                <div className="right"></div>
             </div>
         </div>
     )
