5eb28089e1d8773ba40039aed2bad8aabc4f5b50
Precious <Preshomondi@gmail.com>
2022-10-14 05:36:08 +0300

    create femaleUrls and maleUrls

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -1,9 +1,25 @@
 const quotes = [
   "Work on you for you",
-  "I work out because I think my great personality could use a banging body",
+  "I could use a banging body",
   "Quit the shit, get fit!",
   "Find your fire",
   "Be stronger than your excuse"
 ]
 
-export {quotes}
+const femaleUrls = [
+  "https://images.pexels.com/photos/6455925/pexels-photo-6455925.jpeg?auto=compress&cs=tinysrgb&w=400",
+  "https://images.pexels.com/photos/13451901/pexels-photo-13451901.jpeg?auto=compress&cs=tinysrgb&w=400",
+  "https://images.pexels.com/photos/5669174/pexels-photo-5669174.jpeg?auto=compress&cs=tinysrgb&w=400",
+  "https://images.pexels.com/photos/7991692/pexels-photo-7991692.jpeg?auto=compress&cs=tinysrgb&w=400"
+]
+
+const maleUrls =[
+  "https://images.pexels.com/photos/7991680/pexels-photo-7991680.jpeg?auto=compress&cs=tinysrgb&w=400",
+  "https://images.pexels.com/photos/38630/bodybuilder-weight-training-stress-38630.jpeg?auto=compress&cs=tinysrgb&w=400",
+  "https://images.pexels.com/photos/1978505/pexels-photo-1978505.jpeg?auto=compress&cs=tinysrgb&w=400",
+  "https://images.pexels.com/photos/4720268/pexels-photo-4720268.jpeg?auto=compress&cs=tinysrgb&w=400"
+
+]
+
+
+export {quotes, femaleUrls, maleUrls}
