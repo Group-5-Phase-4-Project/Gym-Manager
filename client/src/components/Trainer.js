172ff6c7aa69b58687f08f4adc27ae47a9ec6dd9
Precious <Preshomondi@gmail.com>
2022-10-14 05:36:45 +0300

    add styling to Trainer component

--- client/src/components/Trainer.js
+++ client/src/components/Trainer.js
@@ -1,6 +1,7 @@
 
 import React, { useState, useEffect } from "react"
 import "./Trainer.css"
+import {femaleUrls, maleUrls} from "./data.js"
 
 
 function Trainer() {
@@ -23,7 +24,7 @@ function Trainer() {
                     <div className="trainer-card">
                     <div className="trainer-profile">
                         <div className="trainer-image">
-                            <img src="https://images.pexels.com/photos/7991680/pexels-photo-7991680.jpeg?auto=compress&cs=tinysrgb&w=400" />
+                            <img src={trainer.name === "Maria" || trainer.name == "Kefiya" || trainer.name == "Zara" || trainer.name == "Arya" ? femaleUrls[Math.floor(Math.random() * femaleUrls.length)]: maleUrls[Math.floor(Math.random() * maleUrls.length)]} />
                         </div>
                         <h2>{trainer.name}</h2>
                         <p className="trainer-specialty">{trainer.specialization}</p>
