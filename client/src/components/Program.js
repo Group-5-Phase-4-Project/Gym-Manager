52613e9a86be45960c46978e35b7d4f8e27ab0be
Precious <Preshomondi@gmail.com>
2022-10-14 05:14:06 +0300

    restyle Programs component

--- client/src/components/Program.js
+++ client/src/components/Program.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react"
 import "./Program.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function Program(){
     const [programs, setPrograms] = useState([])
@@ -22,7 +24,12 @@ function Program(){
                  return(
                     <div className="panel" key={program.id}>
                     <img src={program.image}/>
+                    <div className="panel-name">
                     <h3>{program.name}</h3>
+                    <FontAwesomeIcon icon={faPlus} className="icon"/>
+                    <p>Time: {program.duration} minutes</p>
+                    </div>
+                    
                     <p className="description">
                         {program.description}
                     </p>
