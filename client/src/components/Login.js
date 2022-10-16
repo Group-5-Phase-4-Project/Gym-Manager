feef3b45e94d7b46b994a50e3e5b1e6264b7d77e
Precious <Preshomondi@gmail.com>
2022-10-16 10:04:39 +0300

    add states and functions to Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,40 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import "./Login.css"
+import {quotes} from "./data"
+import { useHistory } from "react-router-dom"
 
-function Login() {
+function Login({getUser}) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [user, setUser] = useState({})
+
+    let history = useHistory
+    function handleLogin(e){
+        e.preventDefault()
+        if (user.password === password) {
+            history.push("/home/main")
+        }else {
+            alert("Incorrect Password")
+        }
+    }
+
+    function handleFetch(){
+        fetch('/trainees')
+        .then(r => r.json())
+        .then(user => {
+            setUser(user)
+            getUser(user)
+        })
+    }
+
     return (
-        <div className="login">
+        <div className="login" style={{ backgroundImage: `url("https://images.pexels.com/photos/4944311/pexels-photo-4944311.jpeg?auto=compress&cs=tinysrgb&w=400")` }}>
             <div className="left-login">
                 <div className="left-login-container">
-                    <h1>Just Gym-It</h1>
+                    <h1 onSubmit={handleLogin}>Just Gym-It</h1>
                     <form>
-                        <input type="text" placeholder="Username" name="name" />
-                        <input type="password" placeholder="Password" name="password" />
+                        <input type="text" placeholder="Username" value={username}  onChange={(e)=> setUsername(e.target.value)} onBlur={handleFetch} autoComplete="off"/>
+                        <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
                         <button className="signup-btn">Login</button>
                     </form>
                     <p >Sign Up Here</p>
@@ -20,6 +45,7 @@ function Login() {
                 <div className="right-container">
                     <div className="right-text">
                         <span>“</span>
+                        <h2>{quotes[Math.floor(Math.random() * quotes.length)]}</h2>
                         
                     </div>
 
