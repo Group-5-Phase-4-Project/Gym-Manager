fd82739e3ffb9853809c62f46a7a1a0d9d092075
Precious <Preshomondi@gmail.com>
2022-10-17 00:51:58 +0300

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,14 @@ Rails.application.routes.draw do
   resources :trainers, only:[:index, :show]
   resources :programs, only:[:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post 'sessions', to: "session#create"
+  post 'logout', to: "session#destroy"
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index'
-  get 'sign_up', to: 'registration#index'
-  get 'sign_up', to: 'registrations#new'
-  post 'sign_up', to: 'registrations#create'
-  get 'sign_in', to: 'sessions#new'
-  post 'sign_in', to: 'sessions#create', as: 'log_in'
+  post 'signup', to: 'trainees#create' 
+  post 'login', to: 'sessions#create', as: 'log_in'
   delete 'logout', to: 'sessions#destroy'
   get 'password', to: 'passwords#edit', as: 'edit_password'
   patch 'password', to: 'passwords#update'
@@ -20,4 +19,5 @@ Rails.application.routes.draw do
   post 'password/reset', to: 'password_resets#create'
   get 'password/reset/edit', to: 'password_resets#edit'
   patch 'password/reset/edit', to: 'password_resets#update'
+  get 'me', to: 'trainees#show'
 end
